bc3ae646bcd98de7e7e2092e0a38e0869c007e92
silviolimeira <sicemal@gmail.com>
2020-02-25 15:12:28 -0300

    Subscribing to Observables with Observers: Canceling Observable Execution with a Subscription

--- index.ts
+++ index.ts
@@ -2,13 +2,25 @@ import { allBooks, Book, allReaders } from "./data";
 import { ajax, AjaxResponse } from "rxjs/ajax";
 import { Observable, of, from, fromEvent, concat, interval } from "rxjs";
 
-// Subscribing to Observables with Observers: Canceling Observable Execution with Subscription
+// Subscribing to Observables with Observers: Canceling Observable Execution with Subscription 1
 // https://app.pluralsight.com/course-player?clipId=33c6a066-6b37-43cf-9982-a53c596f07d4
 
 let timesDiv = document.getElementById("times");
 let button = document.getElementById("timerButton");
 
-let timer$ = interval(1000);
+//let timer$ = interval(1000);
+
+let timer$ = new Observable(subscriber => {
+  let i = 0;
+  let intervalID = setInterval(() => {
+    subscriber.next(i++);
+  }, 1000);
+
+  return () => {
+    console.log("Executing teardown code.");
+    clearInterval(intervalID);
+  };
+});
 
 let timerSubscription = timer$.subscribe(
   value =>
