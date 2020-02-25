5a5229f4a54c8c2cdd611ec0d494a93b4f632f5d
silviolimeira <sicemal@gmail.com>
2020-02-25 20:50:15 -0300

    Using Operators: Controling the Number of Values Produced

--- index.ts
+++ index.ts
@@ -1,16 +1,40 @@
 import { allBooks, Book, allReaders } from "./data";
 import { ajax, AjaxResponse } from "rxjs/ajax";
-import { Observable, of, from, fromEvent, concat, interval } from "rxjs";
-import { map, filter, mergeMap, tap } from "rxjs/operators";
+import {
+  Observable,
+  of,
+  from,
+  fromEvent,
+  concat,
+  interval,
+  pipe,
+  throwError
+} from "rxjs";
+import { map, filter, mergeMap, tap, catchError, take, takeUntil } from "rxjs/operators";
 
-// Using Operators: Importing and Using Common Operators
-// https://app.pluralsight.com/course-player?clipId=14be420c-3855-4457-ad37-b37eaac9cd28
+// Using Operators: Controling the Number of Values Produced
+// https://app.pluralsight.com/course-player?clipId=a3a6fd9c-2370-449b-a798-26a85bc67505
 
-ajax('/api/books')
-    .pipe(
-        mergeMap(ajaxResponse => ajaxResponse.response),
-        filter(book => book.publicationYear < 1950),
-        tap(oldBook => console.log(`Title: ${oldBook.title}`))
-    ).subscribe(
-        finalValue => console.log(finalValue);
-    )
+let timesDiv = document.getElementById("times");
+let button = document.getElementById("timerButton");
+
+let timer$ = new Observable(subscriber => {
+  let i = 0;
+  let intervalID = setInterval(() => {
+    subscriber.next(i++);
+  }, 1000);
+
+  return () => {
+    console.log("Executing teardown code.");
+    clearInterval(intervalID);
+  };
+});
+
+let cancelTimer$ = fromEvent(button, 'click');
+
+timer$.pipe(takeUntil(cancelTimer$)).subscribe(
+  value =>
+    (timesDiv.innerHTML += `${new Date().toLocaleTimeString()} (${value}) <br>`),
+  null,
+  () => console.log("All done!")
+);
