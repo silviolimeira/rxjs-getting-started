5dc6e4ea5b47a6fec514d851ffc2b56c04d2b335
silviolimeira <sicemal@gmail.com>
2020-02-25 15:29:13 -0300

    Using Operators: Applying Operators

--- index.ts
+++ index.ts
@@ -1,39 +1,18 @@
 import { allBooks, Book, allReaders } from "./data";
 import { ajax, AjaxResponse } from "rxjs/ajax";
 import { Observable, of, from, fromEvent, concat, interval } from "rxjs";
+import { map, filter } from "rxjs/operators";
 
-// Subscribing to Observables with Observers: Canceling Observable Execution with Subscription 1
-// https://app.pluralsight.com/course-player?clipId=33c6a066-6b37-43cf-9982-a53c596f07d4
+// Using Operators: Applying Operators
+// https://app.pluralsight.com/course-player?clipId=df9a350e-769f-4110-8c81-c732fa8a75db
 
-let timesDiv = document.getElementById("times");
-let button = document.getElementById("timerButton");
+let source$ = of(1, 2, 3, 4, 5);
 
-//let timer$ = interval(1000);
-
-let timer$ = new Observable(subscriber => {
-  let i = 0;
-  let intervalID = setInterval(() => {
-    subscriber.next(i++);
-  }, 1000);
-
-  return () => {
-    console.log("Executing teardown code.");
-    clearInterval(intervalID);
-  };
-});
-
-let timerSubscription = timer$.subscribe(
-  value =>
-    (timesDiv.innerHTML += `${new Date().toLocaleTimeString()} (${value}) <br>`),
-  null,
-  () => console.log("All done!")
-);
-
-let timerConsoleSubscription = timer$.subscribe(value =>
-  console.log(`${new Date().toLocaleTimeString()} (${value})`)
-);
-
-timerSubscription.add(timerConsoleSubscription);
-// timerSubscription.remove(timerSubscription);
-
-fromEvent(button, "click").subscribe(event => timerSubscription.unsubscribe());
+source$
+  .pipe(
+    map(value => value * 2),
+    filter(mappedValue => mappedValue > 5)
+  )
+  .subscribe(
+    finalValue => console.log(finalValue) // 6, 8, 10
+  );
