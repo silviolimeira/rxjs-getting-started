d8f9411ab71fc346eca987027e327436cf4a1245
silviolimeira <sicemal@gmail.com>
2020-02-25 15:41:27 -0300

    Using Operators: Importing and Using Common Operators

--- index.ts
+++ index.ts
@@ -1,18 +1,16 @@
 import { allBooks, Book, allReaders } from "./data";
 import { ajax, AjaxResponse } from "rxjs/ajax";
 import { Observable, of, from, fromEvent, concat, interval } from "rxjs";
-import { map, filter } from "rxjs/operators";
+import { map, filter, mergeMap, tap } from "rxjs/operators";
 
-// Using Operators: Applying Operators
-// https://app.pluralsight.com/course-player?clipId=df9a350e-769f-4110-8c81-c732fa8a75db
+// Using Operators: Importing and Using Common Operators
+// https://app.pluralsight.com/course-player?clipId=14be420c-3855-4457-ad37-b37eaac9cd28
 
-let source$ = of(1, 2, 3, 4, 5);
-
-source$
-  .pipe(
-    map(value => value * 2),
-    filter(mappedValue => mappedValue > 5)
-  )
-  .subscribe(
-    finalValue => console.log(finalValue) // 6, 8, 10
-  );
+ajax('/api/books')
+    .pipe(
+        mergeMap(ajaxResponse => ajaxResponse.response),
+        filter(book => book.publicationYear < 1950),
+        tap(oldBook => console.log(`Title: ${oldBook.title}`))
+    ).subscribe(
+        finalValue => console.log(finalValue);
+    )
