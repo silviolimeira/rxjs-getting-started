77087a91acf946fed0502d4949096ea4027f021d
silviolimeira <sicemal@gmail.com>
2020-02-25 22:00:34 -0300

    Using Subjects and Multicasted Observables: Using to Covert an Obsrevable from Cold to Hot - example cold observable

--- index.ts
+++ index.ts
@@ -8,7 +8,8 @@ import {
   concat,
   interval,
   pipe,
-  throwError
+  throwError,
+  Subject
 } from "rxjs";
 import {
   map,
@@ -21,52 +22,20 @@ import {
   flatMap
 } from "rxjs/operators";
 
-// Creating You Own Operator: Creating New Operators from Existing Operators
-// https://app.pluralsight.com/course-player?clipId=2f55540c-66c1-4b29-b658-52ea0911373e
+// Using Subjects and Multicasted Observables: Using Subject to Convert an Observable from Cold to Hot
+//app.pluralsight.com/course-player?clipId=5e57c874-a0fc-4737-955d-b7b5f6733d99
+// Using Subjects and Multicasted Observables
 
-function grabAndLogClassics(year, log) {
-  return source$ => {
-    return new Observable(subscriber => {
-      return source$.subscribe(
-        book => {
-          if (book.publicationYear < year) {
-            subscriber.next(book);
-            if (log) {
-              console.log(`Classic: ${book.title}`);
-            }
-          }
-        },
-        err => subscriber.error(err),
-        () => subscriber.complete()
-      );
-    });
-  };
-}
+https: // example cold observable
 
-function grabClassics(year) {
-  return filter(book => book.publicationYear < year);
-}
+let source$ = interval(1000).pipe(take(4));
 
-function grabAndLogClassicsWithPipe(year, log) {
-  return source$ =>
-    source$.pipe(
-      filter(book => book.publicationYear < year),
-      tap(classicBook =>
-        log ? console.log(`Title: ${classicBook.title}`) : null
-      )
-    );
-}
+source$.subscribe(value => console.log(`Observer 1: ${value}`));
 
-ajax("/api/books")
-  .pipe(
-    flatMap(ajaxResponse => ajaxResponse.response),
-    // filter(book => book.publicationYear < 1950),
-    // tap(oldBook => console.log(`Title: ${oldBook.title}`))
-    // grabAndLogClassics(1950, true)
-    //grabClassics(1950)
-    grabAndLogClassicsWithPipe(1950, true)
-  )
-  .subscribe(
-    finalValue => console.log(`VALUE: ${finalValue.title}`),
-    error => console.log(`ERROR: ${error}`)
-  );
+setTimeout(() => {
+  source$.subscribe(value => console.log(`Observer 2: ${value}`));
+}, 1000);
+
+setTimeout(() => {
+  source$.subscribe(value => console.log(`Observer 3: ${value}`));
+}, 2000);
